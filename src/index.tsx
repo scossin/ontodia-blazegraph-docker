b07bcc3e88f48537e0a1d8ba677874c1d868bd5b
Yury Emelyanov <yury.emelyanov@gmail.com>
2018-02-22 13:13:39 +0300

    Different diagram for OWL

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,12 @@
 import { createElement, ClassAttributes } from 'react';
 import * as ReactDOM from 'react-dom';
 
-import { Workspace, WorkspaceProps, SparqlDataProvider, OWLStatsSettings, OWLRDFSSettings, SparqlQueryMethod } from 'ontodia';
+import {
+    Workspace, WorkspaceProps, SparqlDataProvider, OWLStatsSettings, OWLRDFSSettings, SparqlQueryMethod,
+    LinkModel,
+} from 'ontodia';
 import { saveLayoutToLocalStorage, tryLoadLayoutFromLocalStorage } from './localstorage';
+import {LinkConfiguration} from 'ontodia/ontodia/data/sparql/sparqlDataProviderSettings';
 
 
 function onWorkspaceMounted(workspace: Workspace) {
@@ -10,20 +14,87 @@ function onWorkspaceMounted(workspace: Workspace) {
 
     const model = workspace.getModel();
 
+    const p = 'http://ontodia.org/schema#';
+
+    const linkConfigurations: LinkConfiguration[] = [
+        {
+            id: `${p}mapsTo`,
+            inverseId: `${p}mapsFrom`,
+            path: `?property rdfs:domain $source; rdfs:range $target.`,
+            properties: `?property rdfs:label ?propValue. BIND(rdfs:label as ?propType)`
+        },
+        {
+            id: `${p}subClassOf`,
+            inverseId: `${p}parentClassOf`,
+            path: `$source rdfs:subClassOf $target.`,
+        }
+    ];
+
+    const resolveLinkTemplates = (linkTypeId: string) => {
+        return ['${p}mapsTo', '${p}mapsFrom'].find(test => test === linkTypeId) == undefined ? undefined : {
+            markerTarget: {
+                fill: '#d7e1ec',
+            },
+            renderLink: (link: LinkModel) => {
+                const label = link.properties['http://www.w3.org/2000/01/rdf-schema#label'].values[0].text;
+                return {
+                    connection: {
+                        stroke: '#d7e1ec',
+                        'stroke-width': 1,
+                    },
+                    label: {
+                        attrs: {
+                            rect: {fill: '#f8f8f8'},
+                            text: {
+                                fill: '#8CA0B3',
+                                'font-size': '11px',
+                                text: label !== undefined ? [{text: label, lang: ''}] : undefined,
+                            },
+                        },
+                    },
+                };
+            },
+        };
+    };
+    workspace.getDiagram().registerLinkTemplateResolver(resolveLinkTemplates);
+
     const layoutData = tryLoadLayoutFromLocalStorage();
     model.importLayout({
         layoutData,
         validateLinks: true,        
         dataProvider: new SparqlDataProvider({
             endpointUrl: '/sparql-endpoint',
-            imagePropertyUris: [
-                'http://collection.britishmuseum.org/id/ontology/PX_has_main_representation',
-                'http://xmlns.com/foaf/0.1/img',
-            ],
-            queryMethod: SparqlQueryMethod.POST
-        }, OWLRDFSSettings),
+            queryMethod: SparqlQueryMethod.POST,
+        }, {...OWLRDFSSettings, ...{
+                defaultPrefix: OWLRDFSSettings.defaultPrefix + `
+                PREFIX ontodia: <>
+`,
+                linkConfigurations: linkConfigurations,
+                linkTypesOfQuery: `
+        SELECT ?link (count(distinct ?outObject) as ?outCount) (count(distinct ?inObject) as ?inCount)
+        WHERE {
+                \${linkConfigurations}        
+            } group by ?link           
+    `,
+                linkTypesStatisticsQuery: `
+        SELECT ?link (count(distinct ?outObject) as ?outCount) (count(distinct ?inObject) as ?inCount)
+        WHERE {
+           \${linkConfigurations}   
+        } GROUP BY ?link
+    `,
+                linksInfoQuery: `SELECT ?source ?type ?target ?propType ?propValue
+                WHERE { 
+                    VALUES (?source) {\${ids}}
+                    VALUES (?target) {\${ids}}
+                    \${linkConfigurations} 
+                }                
+            `,
+        }
+        }),
     });
 }
+
+
 
 const props: WorkspaceProps & ClassAttributes<Workspace> = {
     ref: onWorkspaceMounted,
