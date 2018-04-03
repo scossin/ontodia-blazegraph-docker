75c0ca2f4ba7a7a3e6866db180d6570879b2b7dd
Yury Emelyanov <yury.emelyanov@gmail.com>
2018-04-03 12:19:30 +0300

    Fibo ontology showcase

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,10 @@ import * as ReactDOM from 'react-dom';
 
 import {
     Workspace, WorkspaceProps, SparqlDataProvider, OWLStatsSettings, OWLRDFSSettings, SparqlQueryMethod,
-    LinkModel,
+    LinkModel, SparqlGraphBuilder, LinkConfiguration
 } from 'ontodia';
 import { saveLayoutToLocalStorage, tryLoadLayoutFromLocalStorage } from './localstorage';
-import {LinkConfiguration} from 'ontodia/ontodia/data/sparql/sparqlDataProviderSettings';
+import {LayoutData} from "ontodia/ontodia/diagram/layoutData";
 
 
 function onWorkspaceMounted(workspace: Workspace) {
@@ -40,43 +40,69 @@ function onWorkspaceMounted(workspace: Workspace) {
     };
     workspace.getDiagram().registerLinkTemplateResolver(resolveLinkTemplates);
 
-    const layoutData = tryLoadLayoutFromLocalStorage();
-    model.importLayout({
-        layoutData,
-        validateLinks: true,        
-        dataProvider: new SparqlDataProvider({
-            endpointUrl: '/sparql-endpoint',
-            queryMethod: SparqlQueryMethod.POST,
-        }, {...OWLRDFSSettings, ...{
-                defaultPrefix: OWLRDFSSettings.defaultPrefix + `
-                PREFIX ontodia: <>
+    let sparqlDataProvider = new SparqlDataProvider({
+        endpointUrl: '/sparql-endpoint',
+        queryMethod: SparqlQueryMethod.POST,
+    }, {...OWLRDFSSettings, ...{
+            defaultPrefix: OWLRDFSSettings.defaultPrefix + `
+            PREFIX ontodia: <>
 `,
-                linkConfigurations: linkConfigurations,
-                linkTypesOfQuery: `
-        SELECT ?link (count(distinct ?outObject) as ?outCount) (count(distinct ?inObject) as ?inCount)
-        WHERE {
-                \${linkConfigurations}        
-            } group by ?link           
-    `,
-                linkTypesStatisticsQuery: `
-        SELECT ?link (count(distinct ?outObject) as ?outCount) (count(distinct ?inObject) as ?inCount)
-        WHERE {
-           \${linkConfigurations}   
-        } GROUP BY ?link
-    `,
-                linksInfoQuery: `SELECT ?source ?type ?target ?propType ?propValue
-                WHERE { 
-                    VALUES (?source) {\${ids}}
-                    VALUES (?target) {\${ids}}
-                    \${linkConfigurations} 
-                }                
-            `,
+            linkConfigurations: linkConfigurations,
+            linkTypesOfQuery: `
+    SELECT ?link (count(distinct ?outObject) as ?outCount) (count(distinct ?inObject) as ?inCount)
+    WHERE {
+            \${linkConfigurations}        
+        } group by ?link           
+`,
+            linkTypesStatisticsQuery: `
+    SELECT ?link (count(distinct ?outObject) as ?outCount) (count(distinct ?inObject) as ?inCount)
+    WHERE {
+       \${linkConfigurations}   
+    } GROUP BY ?link
+`,
+            linksInfoQuery: `SELECT ?source ?type ?target ?propType ?propValue
+            WHERE { 
+                VALUES (?source) {\${ids}}
+                VALUES (?target) {\${ids}}
+                \${linkConfigurations} 
+            }                
+        `,
         }
-        }),
     });
+
+    const savedLayoutData = tryLoadLayoutFromLocalStorage();
+
+    let loadingGraph;
+
+    if (savedLayoutData) {
+        loadingGraph = Promise.resolve({layoutData: savedLayoutData, preloadedElements: {}});
+    } else {
+        const graphBuilder = new SparqlGraphBuilder(sparqlDataProvider);
+        loadingGraph = graphBuilder.getGraphFromConstruct(
+            `CONSTRUCT { ?s a ?type } WHERE {?s a ?type VALUES ?type {owl:Class rdfs:Class}}`,
+        );
+        workspace.showWaitIndicatorWhile(loadingGraph);
+    }
+
+    loadingGraph.then(({layoutData, preloadedElements}) => {
+        return model.importLayout({
+            layoutData: removeLinks(layoutData),
+            validateLinks: false,
+            dataProvider: sparqlDataProvider,
+        });
+    }).then(() => {
+        if (!savedLayoutData) {
+            window.setTimeout(() => {
+                workspace.forceLayout();
+                workspace.zoomToFit();
+            }, 1000);
+        }
+    })
 }
 
-
+function removeLinks(layoutData: LayoutData): LayoutData {
+    return {cells: layoutData.cells.filter(cell=>cell.type=='element')};
+}
 
 const props: WorkspaceProps & ClassAttributes<Workspace> = {
     ref: onWorkspaceMounted,
