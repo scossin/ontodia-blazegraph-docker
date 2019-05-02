e11f99ffac6140894c08ae2e328dfffd3c961cf3
Yury Emelyanov <yury.emelyanov@gmail.com>
2019-05-02 16:51:29 +0300

    Update to ontodia 0.9.8

--- src/localstorage.tsx
+++ src/localstorage.tsx
@@ -1,19 +1,27 @@
-import { LayoutData } from "ontodia";
+import {convertToSerializedDiagram, LayoutData, SerializedDiagram} from "ontodia";
 
-export function tryLoadLayoutFromLocalStorage(): LayoutData | undefined {
+export function tryLoadLayoutFromLocalStorage(): SerializedDiagram | undefined {
     if (window.location.hash.length > 1) {
         try {
             const key = window.location.hash.substring(1);
             const unparsedLayout = localStorage.getItem(key);
-            return unparsedLayout && JSON.parse(unparsedLayout);
+            const entry = unparsedLayout && JSON.parse(unparsedLayout);
+
+            // backward compatibility test. If we encounder old diagram,
+            // wrap it into Diagram interface, jsonld - pass through
+            if (entry['@context']) {
+                return entry;
+            } else {
+                return convertToSerializedDiagram({layoutData: entry, linkTypeOptions: []});
+            }
         } catch (e) { /* ignore */ }
     }
     return undefined;
 }
 
-export function saveLayoutToLocalStorage(layout: LayoutData): string {
+export function saveLayoutToLocalStorage(diagram: SerializedDiagram): string {
     const randomKey = Math.floor((1 + Math.random()) * 0x10000000000)
         .toString(16).substring(1);
-    localStorage.setItem(randomKey, JSON.stringify(layout));
+    localStorage.setItem(randomKey, JSON.stringify(diagram));
     return randomKey;
 }
--- typings/ontodia/index.d.ts
+++
@@ -1,2 +0,0 @@
-/// <reference path="../../node_modules/ontodia/typings/local-modules/n3/index.d.ts" />
-/// <reference path="../../node_modules/ontodia/typings/local-modules/rdf-ext/index.d.ts" />
