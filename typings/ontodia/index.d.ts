faddcc15b6b805f47a9e7aaf5e50ce98111a79b3
sebastien cossin <sebastien.cossin@u-bordeaux.fr>
2020-03-19 15:08:51 +0100

    path file to index.d.ts changed in newer version of typescript

--- typings/ontodia/index.d.ts
+++ typings/ontodia/index.d.ts
@@ -1,2 +1,2 @@
-/// <reference path="../../node_modules/ontodia/typings/local-modules/n3/index.d.ts" />
-/// <reference path="../../node_modules/ontodia/typings/local-modules/rdf-ext/index.d.ts" />
+/// <reference path="../../node_modules/ontodia/typings/local/n3/index.d.ts" />
+/// <reference path="../../node_modules/ontodia/typings/local/rdf-ext/index.d.ts" />
